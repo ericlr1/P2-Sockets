b4a4510c8f2b486f0138b37530565590f3820ae1
Eric <ericluque2003@gmail.com>
2024-10-17 11:36:33 +0200

    Initial commit

---
+++ Cliente/Client/Assets/Scripts/Client/ClientTCP.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using TMPro;
+using System.Text;
+
+public class ClientTCP : MonoBehaviour
+{
+    public GameObject UItextObj;
+    private TextMeshProUGUI UItext;
+    private string clientText;
+
+    public TMP_InputField inputNickname;
+    public TMP_InputField inputIP;
+    public TMP_InputField inputMessage;
+
+    private string nickname = "Client";  // Default nickname for the client
+    private Socket server;
+
+    void Start()
+    {
+        UItext = UItextObj.GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        UItext.text = clientText;
+    }
+
+    public void StartClient()
+    {
+        nickname = inputNickname.text;  // Use nickname from input
+        string ip = inputIP.text;  // Get IP from input
+        if (string.IsNullOrEmpty(ip)) ip = "127.0.0.1";  // Default to localhost if not provided
+
+        Thread connect = new Thread(() => Connect(ip));
+        connect.Start();
+    }
+
+    void Connect(string ip)
+    {
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(ip), 9050);
+        server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        server.Connect(ipep);
+
+        Thread receiveThread = new Thread(Receive);
+        receiveThread.Start();
+
+        clientText += $"\nConnected to server at {ip}";
+    }
+
+    public void SendMessage()
+    {
+        string message = inputMessage.text;  // Get the message from input field
+        if (server != null && !string.IsNullOrEmpty(message))
+        {
+            string fullMessage = $"{nickname}: {message}";
+            byte[] data = Encoding.ASCII.GetBytes(fullMessage);
+            server.Send(data);
+
+            clientText += $"\nSent: {fullMessage}";
+        }
+    }
+
+    void Receive()
+    {
+        byte[] data = new byte[1024];
+
+        while (true)
+        {
+            try
+            {
+                int recv = server.Receive(data);
+                if (recv > 0)
+                {
+                    string receivedMessage = Encoding.ASCII.GetString(data, 0, recv);
+                    clientText += $"\nReceived: {receivedMessage}";
+                }
+            }
+            catch
+            {
+                break;
+            }
+        }
+    }
+}
---
+++ Servidor/Servidor/Assets/Scripts/Server/ServerUDP.cs
@@ -0,0 +1,70 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+using System.Threading;
+using TMPro;
+
+public class ServerUDP : MonoBehaviour
+{
+    Socket socket;
+    public GameObject UItextObj;
+    TextMeshProUGUI UItext;
+    string serverText;
+
+    void Start()
+    {
+        UItext = UItextObj.GetComponent<TextMeshProUGUI>();
+    }
+
+    public void startServer()
+    {
+        serverText = "Starting UDP Server...";
+
+        // Crear y vincular el socket
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        socket.Bind(ipep);
+
+        // Iniciar el hilo para recibir mensajes
+        Thread newConnection = new Thread(Receive);
+        newConnection.Start();
+    }
+
+    void Update()
+    {
+        UItext.text = serverText;
+    }
+
+    void Receive()
+    {
+        int recv;
+        byte[] data = new byte[1024];
+
+        serverText += "\nWaiting for new Client...";
+
+        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+        EndPoint Remote = (EndPoint)sender;
+
+        while (true)
+        {
+            // Recibir mensaje
+            recv = socket.ReceiveFrom(data, ref Remote);
+            serverText += $"\nMessage received from {Remote.ToString()}: " + Encoding.ASCII.GetString(data, 0, recv);
+
+            // Enviar ping de respuesta
+            Thread sendThread = new Thread(() => Send(Remote));
+            sendThread.Start();
+        }
+    }
+
+    void Send(EndPoint Remote)
+    {
+        string welcome = "Ping";
+        byte[] data = Encoding.ASCII.GetBytes(welcome);
+
+        // Enviar el mensaje de ping al cliente
+        socket.SendTo(data, data.Length, SocketFlags.None, Remote);
+        serverText += $"\nSent to {Remote.ToString()}: {welcome}";
+    }
+}
