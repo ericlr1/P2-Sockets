f793f608b9ce1dc6ee9ed8c27488283c340f7e4a
Eric <ericluque2003@gmail.com>
2024-10-21 16:46:39 +0200

    Client 100%

--- Cliente/Client/Assets/Scripts/Client/ClientUDP.cs
+++ Cliente/Client/Assets/Scripts/Client/ClientUDP.cs
@@ -1,26 +1,29 @@
-﻿using System.Net;
+﻿using UnityEngine;
+using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using UnityEngine;
 using System.Threading;
 using TMPro;
+using System.Text;
 
 public class ClientUDP : MonoBehaviour
 {
-    Socket socket;
     public GameObject UItextObj;
-    TextMeshProUGUI UItext;
-    string clientText;
+    private TextMeshProUGUI UItext;
+    private string clientText;
+
+    public TMP_InputField inputNickname;
+    public TMP_InputField inputIP;
+    public TMP_InputField inputMessage;
+
+    private string nickname = "Client";  // Default nickname for the client
+    private UdpClient udpClient;
+    private IPEndPoint serverEndPoint;
+
+    private Thread receiveThread;
 
     void Start()
     {
         UItext = UItextObj.GetComponent<TextMeshProUGUI>();
-    }
-
-    public void StartClient()
-    {
-        Thread mainThread = new Thread(Send);
-        mainThread.Start();
     }
 
     void Update()
@@ -28,32 +31,77 @@ public class ClientUDP : MonoBehaviour
         UItext.text = clientText;
     }
 
-    void Send()
+    public void StartClient()
     {
-        IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
-        socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        nickname = inputNickname.text;  // Use nickname from input
+        string ip = inputIP.text;  // Get IP from input
+        if (string.IsNullOrEmpty(ip)) ip = "127.0.0.1";  // Default to localhost if not provided
 
-        byte[] data = new byte[1024];
-        string handshake = "Hello World -- Connexion";
+        // Initialize UDP client and server endpoint
+        udpClient = new UdpClient();
+        serverEndPoint = new IPEndPoint(IPAddress.Parse(ip), 9050);
 
-        data = Encoding.ASCII.GetBytes(handshake);
-        
-        socket.SendTo(data, data.Length, SocketFlags.None, ipep);
+        // Send nickname as the first message to the server
+        SendNickname();
 
-        Thread receive = new Thread(Receive);
-        receive.Start();
+        // Start a thread to listen for incoming messages
+        receiveThread = new Thread(Receive);
+        receiveThread.Start();
+
+        clientText += $"\nConnected to server at {ip}";
+    }
+
+    void SendNickname()
+    {
+        byte[] data = Encoding.ASCII.GetBytes(nickname);
+        udpClient.Send(data, data.Length, serverEndPoint);
+        clientText += $"\nSent nickname: {nickname}";
+    }
+
+    public void SendMessage()
+    {
+        // Send message input from the user to the server
+        string message = inputMessage.text;
+        byte[] data = Encoding.ASCII.GetBytes(message);
+
+        udpClient.Send(data, data.Length, serverEndPoint);
+        clientText += $"\nSent: {message}";
+        inputMessage.text = "";  // Clear input field
     }
 
     void Receive()
     {
-        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
-        EndPoint Remote = (EndPoint)sender;
-        byte[] data = new byte[1024];
+        IPEndPoint fromEndPoint = new IPEndPoint(IPAddress.Any, 0);
 
         while (true)
         {
-            int recv = socket.ReceiveFrom(data, ref Remote);
-            clientText = $"Message received from {Remote}: " + Encoding.ASCII.GetString(data, 0, recv);
+            try
+            {
+                // Listen for incoming data from the server
+                byte[] data = udpClient.Receive(ref fromEndPoint);
+                string receivedMessage = Encoding.ASCII.GetString(data);
+
+                clientText += $"\nReceived: {receivedMessage}";
+            }
+            catch
+            {
+                clientText += "\nError receiving data.";
+                break;
+            }
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        // Stop receiving when the application closes
+        if (receiveThread != null && receiveThread.IsAlive)
+        {
+            receiveThread.Abort();
+        }
+
+        if (udpClient != null)
+        {
+            udpClient.Close();
         }
     }
 }
